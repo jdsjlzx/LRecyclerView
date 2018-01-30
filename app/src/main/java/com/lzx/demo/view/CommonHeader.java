fa7f5e198047274c247536ce262a61c9b41048cd
“jdsjlzx” <jdsjlzx@gmail.com>
2018-01-30 15:16:16 +0800

    增加自定义Header和Footer。

---
+++ app/src/main/java/com/lzx/demo/view/CommonHeader.java
@@ -0,0 +1,38 @@
+package com.lzx.demo.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+import com.github.jdsjlzx.R;
+
+
+/**
+ * RecyclerView的HeaderView，简单的展示一个TextView
+ */
+public class CommonHeader extends RelativeLayout {
+    int resId;
+    public CommonHeader(Context context,int resId) {
+        super(context);
+        init(context,resId);
+    }
+
+    public CommonHeader(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public CommonHeader(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public void init(Context context) {
+
+        inflate(context, R.layout.sample_header, this);
+    }
+    public void init(Context context,int resId) {
+
+        inflate(context, resId, this);
+    }
+}
